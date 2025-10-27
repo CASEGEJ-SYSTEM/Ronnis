ee73b8d3cbf2351719886a0813feef88d3b3ed5a
Jesús García Nigoche <jgarcianigoche@gmail.com>
2025-10-26 22:24:42 -0600

    25w43c • Retoques al panel de admin

--- src/app/features/admin/components/admin-layout/admin-layout.ts
+++ src/app/features/admin/components/admin-layout/admin-layout.ts
@@ -7,5 +7,6 @@ import { AdminHeader } from '../admin-header/admin-header';
     standalone: true,
     imports: [RouterOutlet, AdminHeader],
     templateUrl: './admin-layout.html',
+    styleUrl: './admin-layout.css'
 })
 export class AdminLayout {  }
