a4c9da402c036197dbb878b1a494f43a22734383
SILVESTRE JESUS  GUADALUPE GUTIERREZ <22690406@tecvalles.mx>
2025-11-02 00:10:16 -0600

    Delete frontend directory

--- frontend/src/app/features/landing/components/trainers-section/trainers-section.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-    selector: 'app-trainers-section',
-    templateUrl: './trainers-section.html',
-})
-export class TrainersSection {
-    @Input() trainers: any[] = [];
-}
